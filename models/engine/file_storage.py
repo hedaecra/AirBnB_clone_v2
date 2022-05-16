ba83051058d79aeb42d3f96681c9ecb51a3836db
anfepema700 <anfepema700@github.com>
2022-05-16 17:21:15 -0500

    define dictionary for the objects

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -18,4 +18,12 @@ classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
     __file_path = "file.json"
     # dictionary - empty but will store all objects by <class name>.id
     __objects = {}
-
+    def all(self, cls=None):
+        """returns the dictionary __objects"""
+        if cls is not None:
+            new_dict = {}
+            for key, value in self.__objects.items():
+                if cls == value.__class__ or cls == value.__class__.__name__:
+                    new_dict[key] = value
+            return new_dict
+        return self.__objects
