7f0d9a27c3891ae0e3b8344617c3159183dd6884
hedaecra <3883@holbertonschool.com>
2022-05-16 22:11:14 -0500

    Update delete

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -60,10 +60,11 @@ class FileStorage:
 
     def delete(self, obj=None):
         """delete obj from __objects if it’s inside"""
-        if obj is not None:
-            key = obj.__class__.__name__ + '.' + obj.id
-            if key in self.__objects:
-                del self.__objects[key]
+        if obj:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            if (key, obj) in self.__objects.items():
+                self.__objects.pop(key, None)
+        self.save()
 
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
